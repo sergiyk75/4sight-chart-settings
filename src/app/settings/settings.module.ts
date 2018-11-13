05a018830aaf80562a005be0d94c7ae6a121882f
Sergiy Kharytesku <kharytesku@gmail.com>
2018-11-13 18:05:48 -0500

    Added components Tree

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatCheckboxModule, MatTreeModule, MatInputModule, MatFormFieldModule } from '@angular/material';
 import { SettingsComponent } from './settings.component';
 import { SearchComponent } from './search/search.component';
 import { ComponentsViewComponent } from './components-view/components-view.component';
 import { ComponentSettingsComponent } from './component-settings/component-settings.component';
 import { ComponentsTreeComponent } from './components-tree/components-tree.component';
+import { MaterialModule } from '../material-module.module';
 
 @NgModule({
   declarations: [SettingsComponent, SearchComponent, ComponentsViewComponent, ComponentSettingsComponent, ComponentsTreeComponent],
   imports: [
     CommonModule,
-    MatButtonModule, MatCheckboxModule, MatTreeModule, MatInputModule, MatFormFieldModule,
+    MaterialModule
   ],
 })
 export class SettingsModule { }
